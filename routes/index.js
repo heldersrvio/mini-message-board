b94f616da90c1983e670c5d4677b40d700b143a5
Helder Sérvio <heldersrvio@gmail.com>
2022-03-01 12:23:01 -0300

    FEAT: ADD NEW ROUTE AND FORM; ADD STYLING

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
 /* GET home page. */
 router.get('/', (req, res, _next) => {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', (_req, res, _next) => {
+	res.render('form');
+});
+
+router.post('/new', (req, res, _next) => {
+	messages.push({
+		text: req.body.message,
+		user: req.body.author,
+		added: new Date(),
+	});
+	res.redirect('/');
 });
 
 module.exports = router;
